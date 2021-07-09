cf24a5f2acadf333b9133dfc0b841e036f15b49f
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-07-09 12:58:09 +0100

    Update view behaviours, messages and specs

--- test/controllers/LanguageSwitchControllerSpec.scala
+++ test/controllers/LanguageSwitchControllerSpec.scala
@@ -24,6 +24,8 @@ import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.mvc.Headers
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import uk.gov.hmrc.hmrcfrontend.config.ContactFrontendConfig
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 class LanguageSwitchControllerSpec extends SpecBase {
 
@@ -34,9 +36,11 @@ class LanguageSwitchControllerSpec extends SpecBase {
   private val fakeUrl: String = "fakeUrl"
 
   private lazy val config: Configuration = injector.instanceOf[Configuration]
+  private lazy val servicesConfig: ServicesConfig = injector.instanceOf[ServicesConfig]
+  private lazy val contactConfig = injector.instanceOf[ContactFrontendConfig]
 
   def frontendAppConfig(languageToggleEnabled: Boolean = true): FrontendAppConfig = {
-    new FrontendAppConfig(config) {
+    new FrontendAppConfig(config, servicesConfig, contactConfig) {
       override lazy val languageTranslationEnabled: Boolean = languageToggleEnabled
     }
   }
