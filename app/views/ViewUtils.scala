8c9887b4747249ac4d3bfd2e2b7c31627077b841
slavvco <13876870+slavvco@users.noreply.github.com>
2023-08-21 13:32:17 +0100

    DDCE-4174 | CSP review.

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -37,7 +37,7 @@ object ViewUtils {
         s"${error.key}.${error.args.head}"
       case _ if error.message.toLowerCase.contains("yesno") =>
         s"${error.key}-yes"
-      case _ if radioOptions.size != 0 =>
+      case _ if radioOptions.nonEmpty =>
         radioOptions.head.id
       case _ =>
         val isSingleDateField = isDateError(error.message) && !error.message.toLowerCase.contains("yesno")
