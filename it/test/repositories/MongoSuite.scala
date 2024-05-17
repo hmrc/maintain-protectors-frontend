7dafd75249a117b500dc7d177fd8ba149ec82f01
Ed Cope <87376121+EdCope@users.noreply.github.com>
2024-05-17 10:13:01 +0100

    DDCE-5186: Play 3.0 Migration (#153)
    * DDCE-5186: Update license headers to 2024
    
    * DDCE-5186: Play 3.0 Migration

--- it/test/repositories/MongoSuite.scala
+++ it/test/repositories/MongoSuite.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,8 +29,7 @@ trait MongoSuite extends ScalaFutures {
   val application : Application = new GuiceApplicationBuilder()
     .configure(Seq(
       "metrics.enabled" -> false,
-      "auditing.enabled" -> false,
-      "mongo-async-driver.akka.log-dead-letters" -> 0
+      "auditing.enabled" -> false
     ): _*)
     .build()
 
